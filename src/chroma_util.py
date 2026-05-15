67b6ca9a628af5dad84de56852deb5b4a1ff0f14
craigdbarber <22780469+craigdbarber@users.noreply.github.com>
2026-05-15 14:15:31 -0400

    added cache dir to chroma util and config

--- src/chroma_util.py
+++ src/chroma_util.py
@@ -1,15 +1,16 @@
 """A module providing utility functionality for chromadb."""
 
 from enum import StrEnum
+from typing import Any
 
-import chromadb
+import torch
+from chromadb import Collection, PersistentClient
 from chromadb.api import ClientAPI
 from chromadb.utils import embedding_functions
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from more_itertools import batched
 
 from config_util import NestedDict, get_config_str
-from device_util import retrieve_current_accelerator_type
 
 _CHROMA_MAX_BATCH_SIZE = 166
 
@@ -21,7 +22,7 @@ class ChromaType(StrEnum):
     REMOTE = "remote"
 
 
-def chroma_type_from_str(value: str) -> ChromaType:
+def _chroma_type_from_str(value: str) -> ChromaType:
     """Parse a configuration chroma type from the specified string.
     Raises a ValueError if undefined.
 
@@ -52,48 +53,49 @@ class ChromaClient:
         # instance variables
         self._client: ClientAPI
         self._embedding_model: SentenceTransformerEmbeddingFunction
+        self._model_cache_dir: str
 
         # load the config
         chroma_type = get_config_str(config, "type")
         assert chroma_type is not None
-        db_type = chroma_type_from_str(chroma_type)
+        db_type = _chroma_type_from_str(chroma_type)
         if db_type == ChromaType.LOCAL:
             persist_dir = get_config_str(config, "persist_directory")
             assert persist_dir is not None
-            self._client = chromadb.PersistentClient(path=persist_dir)
+            self._client = PersistentClient(path=persist_dir)
         else:
             raise NotImplementedError(f"Chroma type not supported: {chroma_type}")
         # TODO: impemented support for remote type
 
         embedding_model = get_config_str(config, "embedding_model")
         assert embedding_model is not None
-        self._embedding_func = get_embedding_function(embedding_model)
+        model_cache_dir = get_config_str(config, "model_cache_directory")
+        assert model_cache_dir is not None
+        self._embedding_func = _get_embedding_function(embedding_model, model_cache_dir)
 
     def get_or_create_collection(
-        self, name: str, embedding_model: str, distance_func: str = ""
-    ) -> chromadb.Collection:
+        self, name: str, distance_func: str = ""
+    ) -> Collection:
         """Get or create a collection.
 
         Args:
             name: The name of the collection.
-            embedding_model: The name of the embedding model to utilize.
             distance_func: The name of the distance function to utilize.
         Returns: The exising or created collection.
 
         """
         metadata = {}
-        embedding_func = get_embedding_function(embedding_model)
         if distance_func != "":
             metadata["hnsw:space"] = distance_func
         return self._client.get_or_create_collection(
             name=name,
-            embedding_function=embedding_func,  # type: ignore # ty: ignore
-            metadata=metadata,  # type: ignore # ty: ignore
+            embedding_function=self._embedding_func,  # type: ignore # ty: ignore
+            metadata=metadata if metadata else None,  # type: ignore # ty: ignore
         )
 
 
 def batched_upsert(
-    collection: chromadb.Collection,
+    collection: Collection,
     documents: list[str],
     metadatas: list[dict],
     ids: list[str],
@@ -118,18 +120,25 @@ def batched_upsert(
         )
 
 
-def get_embedding_function(model: str) -> SentenceTransformerEmbeddingFunction:
+def _get_embedding_function(
+    model: str, model_cache_dir: str
+) -> SentenceTransformerEmbeddingFunction:
     """Create an embedding function with the given model name.
 
     Args:
         model: The name of the embedding model to be used.
+        model_cache_dir: The directory to be used for model caching.
 
     Returns: The embedding function for the specified model name.
 
     """
-    device = retrieve_current_accelerator_type()
-    if device != "":
-        return embedding_functions.SentenceTransformerEmbeddingFunction(
-            model_name=model, device=device
-        )
-    return embedding_functions.SentenceTransformerEmbeddingFunction(model_name=model)
+    kwargs: dict[str, Any] = {
+        "cache_folder": model_cache_dir,
+        "token": False,
+    }
+    system_device = torch.accelerator.current_accelerator(check_available=True)
+    if system_device is not None:
+        kwargs["device"] = str(system_device)
+    return embedding_functions.SentenceTransformerEmbeddingFunction(
+        model_name=model, **kwargs
+    )
